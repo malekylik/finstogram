2f7475ba6c90449cf9fcbd9df1799ac03cc3399e
malekylik <malekylik@gmail.com>
2024-04-20 13:45:57 -0400

    Add loading user name

--- src/Main.elm
+++ src/Main.elm
@@ -5,6 +5,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Json.Decode exposing (Decoder, map2, field, string, decodeString)
+import Http
 
 -- MAIN
 
@@ -25,18 +26,23 @@ port messageReceiver : (String -> msg) -> Sub msg
 
 -- MODEL
 
+type alias UserInfo =
+    { id : String
+    , name : String
+    }
+
 type alias FacebookAuthInfo =
     { userID : String
     , accessToken : String
     }
 
 type alias Model =
-  { facebookAuthInfo : FacebookAuthInfo }
+  { facebookAuthInfo : FacebookAuthInfo, userInfo : UserInfo }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-  ( { facebookAuthInfo = FacebookAuthInfo "" "" }
+  ( { facebookAuthInfo = FacebookAuthInfo "" "", userInfo = UserInfo "" ""  }
   , Cmd.none
   )
 
@@ -45,7 +51,8 @@ init _ =
 
 type Msg
   = RecvFCToken String
-  -- | 
+  | RequestUserInfo
+  | RecvUserInfo (Result Http.Error String)
 
 
 -- Use the `sendMessage` port when someone presses ENTER or clicks
@@ -59,6 +66,15 @@ update msg model =
       ( { model | facebookAuthInfo = parseFacebookAuthInfo message }
       , Cmd.none
       )
+    RequestUserInfo -> (model, getUserInfo model.facebookAuthInfo)
+    RecvUserInfo message ->
+      case message of
+        Ok info -> ( { model | userInfo = parseUserInfo info }
+          , Cmd.none
+          )
+        Err _ -> ( model
+          , Cmd.none
+          )
 
 -- SUBSCRIPTIONS
 
@@ -72,6 +88,14 @@ subscriptions _ =
   messageReceiver RecvFCToken
 
 
+-- `https://graph.facebook.com/${authResponse.userID}?access_token=${authResponse.accessToken}`
+
+getUserInfo : FacebookAuthInfo -> Cmd Msg
+getUserInfo authInfo =
+  Http.get
+      { url = "https://graph.facebook.com/" ++ authInfo.userID ++ "?access_token=" ++ authInfo.accessToken
+      , expect = Http.expectString RecvUserInfo
+      }
 
 -- VIEW
 
@@ -82,12 +106,20 @@ view model =
     [ h1 [] [ text "Facebook Token" ]
     , div [] [text ("Access token " ++ model.facebookAuthInfo.accessToken)]
     , div [] [text ("userID " ++  model.facebookAuthInfo.userID)]
+    , div [] [text ("User name " ++ model.userInfo.name)]
+    , button [onClick RequestUserInfo] [text "getUserInfo"]
     ]
 
 parseFacebookAuthInfo : String -> FacebookAuthInfo
 parseFacebookAuthInfo info =
   case (decodeString decodeFacebookAuthInfo info) of
       Err _ -> FacebookAuthInfo "" ""
+      Ok v -> v
+
+parseUserInfo : String -> UserInfo
+parseUserInfo info =
+  case (decodeString decodeUserInfo info) of
+      Err _ -> UserInfo "" ""
       Ok v -> v
 
 
@@ -97,3 +129,9 @@ decodeFacebookAuthInfo =
   map2 FacebookAuthInfo
     (field "userID" string)
     (field "accessToken" string)
+
+decodeUserInfo : Decoder UserInfo
+decodeUserInfo =
+  map2 UserInfo
+    (field "id" string)
+    (field "name" string)
